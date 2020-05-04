b06d6a57e8022322ded59ee3fa26c80d87db1464
Roman Gilg <subdiff@gmail.com>
2020-05-04 20:58:20 +0200

    revert: "fix: enforce correct teardown order"
    Many autotests fail because of the commit.
    
    This reverts commit cef24cf8ba238ee5ff543578b038a06283d4b58d.

--- wayland_server.cpp
+++ wayland_server.cpp
@@ -653,7 +653,7 @@ void WaylandServer::createInternalConnection()
                 return;
             }
             Registry *registry = new Registry(this);
-            EventQueue *eventQueue = new EventQueue(registry);
+            EventQueue *eventQueue = new EventQueue(this);
             eventQueue->setup(m_internalConnection.client);
             registry->setEventQueue(eventQueue);
             registry->create(m_internalConnection.client);
