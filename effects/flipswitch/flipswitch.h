04dc64d9f60318ccafc4c11548d856e77abd2c91
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2020-09-09 16:03:02 +0300

    refactor(effect): port flipswitch away from QTimeline curve shape
    QTimeline::CurveShape has been deprecated in Qt 5.15.

--- effects/flipswitch/flipswitch.h
+++ effects/flipswitch/flipswitch.h
@@ -122,7 +122,7 @@ private:
     EffectWindow* m_selectedWindow;
     QTimeLine m_timeLine;
     QTimeLine m_startStopTimeLine;
-    QTimeLine::CurveShape m_currentAnimationShape;
+    QEasingCurve m_currentAnimationEasingCurve;
     QRect m_screenArea;
     int m_activeScreen;
     bool m_active;
