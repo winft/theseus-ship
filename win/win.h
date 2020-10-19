e8991b35ee70c936751b27e389fe3c29f98fe27a
Roman Gilg <subdiff@gmail.com>
2020-10-19 14:39:05 +0200

    refactor: move functions about screen, desktop, activity to own header
    Move from the main win.h header to a separate header screen.h. That way the
    main header is more organized and we can include only screen.h in other
    files without all other includes.

--- win/win.h
+++ win/win.h
@@ -17,7 +17,7 @@
 #include "decorations/decorationbridge.h"
 #include "effects.h"
 #include "main.h"
-#include "screens.h"
+#include "screen.h"
 #include "shadow.h"
 #include "utils.h"
 #include "virtualdesktops.h"
@@ -261,135 +261,6 @@ bool is_special_window(Win* win)
 {
     return is_desktop(win) || is_dock(win) || is_splash(win) || is_toolbar(win)
         || is_notification(win) || is_critical_notification(win) || is_on_screen_display(win);
-}
-
-template<typename Win>
-bool on_screen(Win* win, int screen)
-{
-    return screens()->geometry(screen).intersects(win->frameGeometry());
-}
-
-template<typename Win>
-bool on_active_screen(Win* win)
-{
-    return on_screen(win, screens()->current());
-}
-
-template<typename Win>
-bool on_all_desktops(Win* win)
-{
-    return kwinApp()->operationMode() == Application::OperationModeWaylandOnly
-            || kwinApp()->operationMode() == Application::OperationModeXwayland
-        // Wayland
-        ? win->desktops().isEmpty()
-        // X11
-        : win->desktop() == NET::OnAllDesktops;
-}
-
-template<typename Win>
-bool on_desktop(Win* win, int d)
-{
-    return (kwinApp()->operationMode() == Application::OperationModeWaylandOnly
-                    || kwinApp()->operationMode() == Application::OperationModeXwayland
-                ? win->desktops().contains(VirtualDesktopManager::self()->desktopForX11Id(d))
-                : win->desktop() == d)
-        || on_all_desktops(win);
-}
-
-template<typename Win>
-bool on_current_desktop(Win* win)
-{
-    return on_desktop(win, VirtualDesktopManager::self()->current());
-}
-
-/**
- * Deprecated, use x11_desktop_ids.
- */
-template<typename Win>
-void set_desktop(Win* win, int desktop)
-{
-    auto const desktops_count = static_cast<int>(VirtualDesktopManager::self()->count());
-    if (desktop != NET::OnAllDesktops) {
-        // Check range.
-        desktop = std::max(1, std::min(desktops_count, desktop));
-    }
-#ifndef KWIN_UNIT_TEST
-    desktop = std::min(desktops_count, win->rules()->checkDesktop(desktop));
-#endif
-
-    QVector<VirtualDesktop*> desktops;
-    if (desktop != NET::OnAllDesktops) {
-        desktops << VirtualDesktopManager::self()->desktopForX11Id(desktop);
-    }
-    win->setDesktops(desktops);
-}
-
-template<typename Win>
-void set_on_all_desktops(Win* win, bool set)
-{
-    if (set == on_all_desktops(win)) {
-        return;
-    }
-
-    if (set) {
-        set_desktop(win, NET::OnAllDesktops);
-    } else {
-        set_desktop(win, VirtualDesktopManager::self()->current());
-    }
-}
-
-template<typename Win>
-QVector<uint> x11_desktop_ids(Win* win)
-{
-    auto const desks = win->desktops();
-    QVector<uint> x11_ids;
-    x11_ids.reserve(desks.count());
-    std::transform(desks.constBegin(),
-                   desks.constEnd(),
-                   std::back_inserter(x11_ids),
-                   [](VirtualDesktop const* vd) { return vd->x11DesktopNumber(); });
-    return x11_ids;
-}
-
-template<typename Win>
-void enter_desktop(Win* win, VirtualDesktop* virtualDesktop)
-{
-    if (win->desktops().contains(virtualDesktop)) {
-        return;
-    }
-    auto desktops = win->desktops();
-    desktops.append(virtualDesktop);
-    win->setDesktops(desktops);
-}
-
-template<typename Win>
-void leave_desktop(Win* win, VirtualDesktop* virtualDesktop)
-{
-    QVector<VirtualDesktop*> currentDesktops;
-    if (win->desktops().isEmpty()) {
-        currentDesktops = VirtualDesktopManager::self()->desktops();
-    } else {
-        currentDesktops = win->desktops();
-    }
-
-    if (!currentDesktops.contains(virtualDesktop)) {
-        return;
-    }
-    auto desktops = currentDesktops;
-    desktops.removeOne(virtualDesktop);
-    win->setDesktops(desktops);
-}
-
-template<typename Win>
-bool on_all_activities(Win* win)
-{
-    return win->activities().isEmpty();
-}
-
-template<typename Win>
-bool on_activity(Win* win, QString const& activity)
-{
-    return on_all_activities(win) || win->activities().contains(activity);
 }
 
 /**
