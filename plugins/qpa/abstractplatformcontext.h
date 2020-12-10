36725aac42e1cf254b9ca1f029e8a60db0d4ac11
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2020-12-10 11:01:12 +0200

    build: prevent EGL headers from including Xlib headers
    One of the annoying things about EGL headers is that they include
    platform headers by default, e.g. on X11, it's Xlib.h, etc.
    
    The problem with Xlib.h is that it uses the define compiler directive to
    declare constants and those constants have very generic names, e.g.
    'None', which typically conflict with enums, etc.
    
    In order to work around bad things coming from Xlib.h, we include
    fixx11.h file that contains some workarounds to redefine some Xlib's
    types.
    
    There's a flag or rather two flags (EGL_NO_PLATFORM_SPECIFIC_TYPES and
    EGL_NO_X11) that are cross-vendor and they can be used to prevent EGL
    headers from including platform specific headers, such as Xlib.h [1]
    
    The benefit of setting those two flags is that you can simply include
    EGL/egl.h or epoxy/egl.h and the world won't explode due to Xlib.h
    
    MESA_EGL_NO_X11_HEADERS is set to support older versions of Mesa.
    
    [1] https://github.com/KhronosGroup/EGL-Registry/pull/111

--- plugins/qpa/abstractplatformcontext.h
+++ plugins/qpa/abstractplatformcontext.h
@@ -22,7 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <epoxy/egl.h>
 #include "fixqopengl.h"
-#include <fixx11h.h>
 #include <qpa/qplatformopenglcontext.h>
 
 namespace KWin
