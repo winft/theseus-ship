59ba58df7e502cc4b7fdfd1b11d97e0d0ecc0d4f
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2020-12-31 10:52:03 +0000

    build: rework Findepoxy.cmake
    This makes Findepoxy.cmake more upstreamable and easier to use.

--- cmake/modules/Findepoxy.cmake
+++ cmake/modules/Findepoxy.cmake
@@ -1,56 +1,83 @@
-# - Try to find libepoxy
-# Once done this will define
+#.rst:
+# Findepoxy
+# ---------
 #
-#  epoxy_FOUND        - System has libepoxy
-#  epoxy_LIBRARY      - The libepoxy library
-#  epoxy_INCLUDE_DIR  - The libepoxy include dir
-#  epoxy_DEFINITIONS  - Compiler switches required for using libepoxy
-#  epoxy_HAS_GLX      - Whether GLX support is available
-
-# Copyright (c) 2014 Fredrik Höglund <fredrik@kde.org>
+# Try to find libepoxy on a Unix system.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. Neither the name of the University nor the names of its contributors
-#    may be used to endorse or promote products derived from this software
-#    without specific prior written permission.
+# This will define the following variables:
 #
-# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-# SUCH DAMAGE.
+# ``epoxy_FOUND``
+#     True if (the requested version of) libepoxy is available
+# ``epoxy_VERSION``
+#     The version of libepoxy
+# ``epoxy_LIBRARIES``
+#     This should be passed to target_link_libraries() if the target is not
+#     used for linking
+# ``epoxy_INCLUDE_DIRS``
+#     This should be passed to target_include_directories() if the target is not
+#     used for linking
+# ``epoxy_DEFINITIONS``
+#     This should be passed to target_compile_options() if the target is not
+#     used for linking
+# ``epoxy_HAS_GLX``
+#     True if GLX support is available
+#
+# If ``epoxy_FOUND`` is TRUE, it will also define the following imported target:
+#
+# ``epoxy::epoxy``
+#     The epoxy library
+#
+# In general we recommend using the imported target, as it is easier to use.
+# Bear in mind, however, that if the target is in the link interface of an
+# exported library, it must be made available by the package config file.
 
-if (NOT WIN32)
-  find_package(PkgConfig)
-  pkg_check_modules(PKG_epoxy QUIET epoxy)
+# SPDX-FileCopyrightText: 2014 Fredrik Höglund <fredrik@kde.org>
+# SPDX-FileCopyrightText: 2020 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+#
+# SPDX-License-Identifier: BSD-3-Clause
 
-  set(epoxy_DEFINITIONS ${PKG_epoxy_CFLAGS})
+find_package(PkgConfig)
+pkg_check_modules(PKG_epoxy QUIET epoxy)
 
-  find_path(epoxy_INCLUDE_DIR NAMES epoxy/gl.h HINTS ${PKG_epoxy_INCLUDEDIR} ${PKG_epoxy_INCLUDE_DIRS})
-  find_library(epoxy_LIBRARY  NAMES epoxy      HINTS ${PKG_epoxy_LIBDIR} ${PKG_epoxy_LIBRARY_DIRS})
-  find_file(epoxy_GLX_HEADER NAMES epoxy/glx.h HINTS ${epoxy_INCLUDE_DIR})
+set(epoxy_VERSION ${PKG_epoxy_VERSION})
+set(epoxy_DEFINITIONS ${PKG_epoxy_CFLAGS})
 
-  if (epoxy_GLX_HEADER STREQUAL "epoxy_GLX_HEADER-NOTFOUND")
+find_path(epoxy_INCLUDE_DIRS
+    NAMES epoxy/gl.h
+    HINTS ${PKG_epoxy_INCLUDEDIR} ${PKG_epoxy_INCLUDE_DIRS}
+)
+find_library(epoxy_LIBRARIES
+    NAMES epoxy
+    HINTS ${PKG_epoxy_LIBDIR} ${PKG_epoxy_LIBRARY_DIRS}
+)
+find_file(epoxy_GLX_HEADER NAMES epoxy/glx.h HINTS ${epoxy_INCLUDE_DIR})
+
+if (epoxy_GLX_HEADER STREQUAL "epoxy_GLX_HEADER-NOTFOUND")
     set(epoxy_HAS_GLX FALSE CACHE BOOL "whether glx is available")
-  else ()
+else ()
     set(epoxy_HAS_GLX TRUE CACHE BOOL "whether glx is available")
-  endif()
-
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(epoxy DEFAULT_MSG epoxy_LIBRARY epoxy_INCLUDE_DIR)
-
-  mark_as_advanced(epoxy_INCLUDE_DIR epoxy_LIBRARY epoxy_HAS_GLX)
 endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(epoxy
+    FOUND_VAR epoxy_FOUND
+    REQUIRED_VARS epoxy_LIBRARIES epoxy_INCLUDE_DIRS
+    VERSION_VAR epoxy_VERSION
+)
+
+if (epoxy_FOUND AND NOT TARGET epoxy::epoxy)
+    add_library(epoxy::epoxy UNKNOWN IMPORTED)
+    set_target_properties(epoxy::epoxy PROPERTIES
+        IMPORTED_LOCATION "${epoxy_LIBRARIES}"
+        INTERFACE_COMPILE_OPTIONS "${epoxy_DEFINITIONS}"
+        INTERFACE_INCLUDE_DIRECTORIES "${epoxy_INCLUDE_DIRS}"
+    )
+endif()
+
+mark_as_advanced(
+    epoxy_DEFINITIONS
+    epoxy_HAS_GLX
+    epoxy_INCLUDE_DIRS
+    epoxy_LIBRARIES
+    epoxy_VERSION
+)
