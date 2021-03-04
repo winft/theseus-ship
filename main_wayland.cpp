96ddd181dfec3d24c5f4ba838a3d1284678c6ab1
Michael Rogers <comwizz2@gmail.com>
2021-03-03 18:35:45 -0600

    fix: destroy compositor before destroying backend
    This changes the process in order to kill the compositor before killing the platform
    it is supposed to be rendering to.
    Currently the platform is shut down first which destroys resources the compositor
    may still be referencing.
    With this change the Compositor is destroyed first, then the platform.

--- main_wayland.cpp
+++ main_wayland.cpp
@@ -156,12 +156,15 @@ ApplicationWayland::~ApplicationWayland()
     delete m_xwayland;
     m_xwayland = nullptr;
 
-    if (auto *platform = kwinApp()->platform()) {
-        platform->prepareShutdown();
-    }
+    destroyCompositor();
 
     waylandServer()->terminateClientConnections();
-    destroyCompositor();
+    if (auto *platform = this->platform()) {
+        // while originally labeled 'prepareShutdown' this function destroys the buffers
+        // used in at least one backend (drm). Moved this to the end so that the missing
+        // outputs do not cause any crashes with the rest of the services.
+        platform->prepareShutdown();
+    }
 }
 
 void ApplicationWayland::performStartup()
