ae3a92680b26229ea1165cd3ab1dda3f0df3bc81
Ismael Asensio <isma.af@gmail.com>
2021-03-20 23:34:15 +0700

    refactor: unify windows properties methods in KCM class
    Put together the two methods about window properties and rules
    into the same class, since they are tightly related and they seem
    a bit out of place within RuleBookModel
    
    Small refactor patch. No behavior changes

--- kcmkwin/kwinrules/rulebookmodel.h
+++ kcmkwin/kwinrules/rulebookmodel.h
@@ -56,8 +56,6 @@ public:
     void load();
     void save();
 
-    QModelIndex findRuleWithProperties(const QVariantMap &info, bool wholeApp) const;
-
 private:
     RuleBookSettings *m_ruleBook;
     QVector<Rules *> m_rules;
