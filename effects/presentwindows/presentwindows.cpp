f90ad31fb05a4ac424c0b9146ec30492bed63c24
David Redondo <kde@david-redondo.de>
2021-03-30 17:51:50 +0200

    feat(effect): add a DBus interface to the presentWindows effect
    The same as for the highlightWindows effect. Toggling it through
    DBus is platform independent.

--- effects/presentwindows/presentwindows.cpp
+++ effects/presentwindows/presentwindows.cpp
@@ -32,6 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <netwm_def.h>
 
 #include <QApplication>
+#include <QDBusConnection>
 #include <QQmlContext>
 #include <QQmlEngine>
 #include <QQuickItem>
@@ -66,6 +67,7 @@ PresentWindowsEffect::PresentWindowsEffect()
 {
     initConfig<PresentWindowsConfig>();
 
+    // TODO KF6 remove atom support
     auto announceSupportProperties = [this] {
         m_atomDesktop = effects->announceSupportProperty("_KDE_PRESENT_WINDOWS_DESKTOP", this);
         m_atomWindows = effects->announceSupportProperty("_KDE_PRESENT_WINDOWS_GROUP", this);
@@ -117,10 +119,17 @@ PresentWindowsEffect::PresentWindowsEffect()
     connect(effects, &EffectsHandler::screenAboutToLock, this, [this]() {
         setActive(false);
     });
+
+    QDBusConnection::sessionBus().registerObject(QStringLiteral("/org/kde/KWin/PresentWindows"),
+                                                 QStringLiteral("org.kde.KWin.PresentWindows"),
+                                                 this,
+                                                 QDBusConnection::ExportScriptableSlots);
+    QDBusConnection::sessionBus().registerService(QStringLiteral("org.kde.KWin.PresentWindows"));
 }
 
 PresentWindowsEffect::~PresentWindowsEffect()
 {
+    QDBusConnection::sessionBus().unregisterService(QStringLiteral("org.kde.KWin.PresentWindows"));
     delete m_filterFrame;
     delete m_closeView;
 }
@@ -935,6 +944,21 @@ void PresentWindowsEffect::slotPropertyNotify(EffectWindow* w, long a)
         setActive(true);
     }
 }
+
+void PresentWindowsEffect::presentWindows(const QStringList &windows)
+{
+    m_selectedWindows.clear();
+    for (const auto &window : windows) {
+        if (auto effectWindow = effects->findWindow(QUuid(window)); effectWindow) {
+            m_selectedWindows.append(effectWindow);
+        } else if (auto effectWindow = effects->findWindow(window.toLong()); effectWindow) {
+            m_selectedWindows.append(effectWindow);
+        }
+    }
+   m_mode = ModeWindowGroup;
+   setActive(true);
+}
+
 
 //-----------------------------------------------------------------------------
 // Window rearranging
