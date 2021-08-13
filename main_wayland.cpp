fc2f3fb00e396036125eb9f19d504dfb2559f125
Roman Gilg <subdiff@gmail.com>
2021-08-13 09:24:56 +0200

    refactor: omit backend check
    The backend always exists at this point in time.

--- main_wayland.cpp
+++ main_wayland.cpp
@@ -183,10 +183,8 @@ seat::session* ApplicationWayland::create_session()
 {
     auto session = new seat::backend::wlroots::session(this);
 
-    if (backend) {
-        if (auto backend_session = wlr_backend_get_session(backend->backend)) {
-            session->native = backend_session;
-        }
+    if (auto backend_session = wlr_backend_get_session(backend->backend)) {
+        session->native = backend_session;
     }
 
     return session;
