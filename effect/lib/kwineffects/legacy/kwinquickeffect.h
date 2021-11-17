da9f2e97aa0c0afceee3f278624c99390fc62817
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2021-11-17 10:43:31 +0200

    feat(effect): add QuickSceneEffect type
    This change moves some pieces of the Overview effect in a new
    QuickSceneEffect class that can be useful for other QtQuick based
    effects.
    
    The QuickSceneEffect class is meant to deal with rendering bits, input
    handling, etc.
    
    As the user of the QuickSceneEffect class, you need to override the
    createView() method. The createView() method is invoked by the
    QuickSceneEffect when it wants to create a QtQuick scene for the given
    screen.
    
    The design of the QuickSceneEffect is based on the Overview effect.

---
+++ effect/lib/kwineffects/legacy/kwinquickeffect.h
@@ -0,0 +1,14 @@
+/*
+    SPDX-FileCopyrightText: 2021 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+// This header is deprecated, but installed for backwards compatibility. Do not include it anymore.
+// Instead directly include the header below.
+#include <kwineffects/quick_scene.h>
+
+// Below header include is preserved for backwards compatibility.
+#include <kwineffects.h>
