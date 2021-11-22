8413e392a19aebdd4e7a1b2056dea3e5c868768c
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2021-11-22 16:44:30 +0200

    feat(effect): add Window View effect
    The Window View effect is a companion effect for the task manager. It
    provides the task manager a way to ask the user to pick a window among
    windows identified by window ids passed to the activate() method.
    
    The main motivation for adding this effect is to provide a QtQuick based
    alternative for the present windows effect, which is needed to kill the
    latter.
    
    This change doesn't extend the overview effect to avoid repeating
    mistakes of the past, i.e. adding too many (unrelated) features to the
    present windows effect.
    
    While the overview effect provides you a way to select windows, it's not
    the only thing that it has. For example, it also allows changing virtual
    desktops, etc.
    
    On the other hand, the task manager doesn't need all of that. It only
    needs to ask the user to pick a window among the specified windows, nothing
    more.
    
    As is, the window view effect is simply WindowHeap with a d-bus api slapped
    on top of it.

---
+++ effect/effects/windowview/main.cpp
@@ -0,0 +1,20 @@
+/*
+    SPDX-FileCopyrightText: 2021 Vlad Zahorodnii <vlad.zahorodnii@kde.org>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "windowvieweffect.h"
+
+#include <kwineffects/effect_plugin_factory.h>
+
+namespace KWin
+{
+
+KWIN_EFFECT_FACTORY_SUPPORTED(WindowViewEffect,
+                              "metadata.json.stripped",
+                              return WindowViewEffect::supported();)
+
+} // namespace KWin
+
+#include "main.moc"
