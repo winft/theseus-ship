c7ec31b7ded6c84f26014c2334f54a7769cbb82a
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2021-11-22 18:16:01 +0200

    feat(effect): allow disabling expo cells
    This allows making specific windows in a WindowHeap static.

--- effect/effects/private/expolayout.h
+++ effect/effects/private/expolayout.h
@@ -76,6 +76,7 @@ class ExpoCell : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(ExpoLayout* layout READ layout WRITE setLayout NOTIFY layoutChanged)
+    Q_PROPERTY(bool enabled READ isEnabled WRITE setEnabled NOTIFY enabledChanged)
     Q_PROPERTY(int naturalX READ naturalX WRITE setNaturalX NOTIFY naturalXChanged)
     Q_PROPERTY(int naturalY READ naturalY WRITE setNaturalY NOTIFY naturalYChanged)
     Q_PROPERTY(int naturalWidth READ naturalWidth WRITE setNaturalWidth NOTIFY naturalWidthChanged)
@@ -92,6 +93,9 @@ class ExpoCell : public QObject
 public:
     explicit ExpoCell(QObject* parent = nullptr);
     ~ExpoCell() override;
+
+    bool isEnabled() const;
+    void setEnabled(bool enabled);
 
     ExpoLayout* layout() const;
     void setLayout(ExpoLayout* layout);
@@ -134,6 +138,7 @@ public Q_SLOTS:
 
 Q_SIGNALS:
     void layoutChanged();
+    void enabledChanged();
     void naturalXChanged();
     void naturalYChanged();
     void naturalWidthChanged();
@@ -147,6 +152,7 @@ Q_SIGNALS:
 
 private:
     QString m_persistentKey;
+    bool m_enabled = true;
     int m_naturalX = 0;
     int m_naturalY = 0;
     int m_naturalWidth = 0;
