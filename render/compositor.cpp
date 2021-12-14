2ad0851ce6084e1a7b7fbae315c93fcba236b836
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2021-12-14 12:20:11 +0200

    fix: create effects handler after letting toplevels setup compositing
    Many effects use the stacking order property of the effects handler in
    their constructors. This means that windows should have compositing
    setup by the time effects are loaded.
    
    After changing how binary effect plugins are loaded, i.e. not queueing
    loading effects, but loading them immediately, some effects broke
    because the effects handler is created before windows setup compositing.
    
    This change attempts to fix those effects by rearranging compositor
    startup code so windows setup compositing first, then create the effects
    pointer.
    
    Co-authored-by: Roman Gilg <subdiff@gmail.com>

--- render/compositor.cpp
+++ render/compositor.cpp
@@ -197,15 +197,6 @@ void compositor::startupWithWorkspace()
         Qt::UniqueConnection);
     setupX11Support();
 
-    // Sets also the 'effects' pointer.
-    platform.createEffectsHandler(this, scene());
-    connect(Workspace::self(), &Workspace::deletedRemoved, scene(), &scene::removeToplevel);
-    connect(effects, &EffectsHandler::screenGeometryChanged, this, &compositor::addRepaintFull);
-    connect(workspace()->stacking_order, &win::stacking_order::unlocked, this, []() {
-        if (auto eff_impl = static_cast<effects_handler_impl*>(effects)) {
-            eff_impl->checkInputWindowStacking();
-        }
-    });
     connect(workspace()->stacking_order,
             &win::stacking_order::changed,
             this,
@@ -220,6 +211,16 @@ void compositor::startupWithWorkspace()
             win::update_shadow(client);
         }
     }
+
+    // Sets also the 'effects' pointer.
+    platform.createEffectsHandler(this, scene());
+    connect(Workspace::self(), &Workspace::deletedRemoved, scene(), &scene::removeToplevel);
+    connect(effects, &EffectsHandler::screenGeometryChanged, this, &compositor::addRepaintFull);
+    connect(workspace()->stacking_order, &win::stacking_order::unlocked, this, []() {
+        if (auto eff_impl = static_cast<effects_handler_impl*>(effects)) {
+            eff_impl->checkInputWindowStacking();
+        }
+    });
 
     m_state = State::On;
     Q_EMIT compositingToggled(true);
