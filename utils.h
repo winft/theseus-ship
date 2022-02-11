e2be0cced75ea05700d05bd0677ece7a15789697
Roman Gilg <subdiff@gmail.com>
2022-02-11 15:54:34 +0100

    refactor: remove utils source file
    The StrutRect class can be defined directly in the header. Reduces complexity.

--- utils.h
+++ utils.h
@@ -1,36 +1,17 @@
-/********************************************************************
- KWin - the KDE window manager
- This file is part of the KDE project.
+/*
+    SPDX-FileCopyrightText: 1999, 2000 Matthias Ettrich <ettrich@kde.org>
+    SPDX-FileCopyrightText: 2003 Lubos Lunak <l.lunak@kde.org>
+    SPDX-FileCopyrightText: 2022 Roman Gilg <subdiff@gmail.com>
 
-Copyright (C) 1999, 2000 Matthias Ettrich <ettrich@kde.org>
-Copyright (C) 2003 Lubos Lunak <l.lunak@kde.org>
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+#include "config-kwin.h"
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*********************************************************************/
-
-#ifndef KWIN_UTILS_H
-#define KWIN_UTILS_H
-
-// cmake stuff
-#include <config-kwin.h>
-#include <kwinconfig.h>
-// kwin
-#include <kwinglobals.h>
+#include "kwinglobals.h"
 #include "win/types.h"
 
-#include <QLoggingCategory>
-#include <QList>
 #include <QPoint>
 #include <QRect>
 #include <climits>
@@ -38,35 +19,53 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 namespace KWin
 {
 
-const QPoint invalidPoint(INT_MIN, INT_MIN);
+QPoint const invalidPoint(INT_MIN, INT_MIN);
 
 enum StrutArea {
     StrutAreaInvalid = 0, // Null
-    StrutAreaTop     = 1 << 0,
-    StrutAreaRight   = 1 << 1,
-    StrutAreaBottom  = 1 << 2,
-    StrutAreaLeft    = 1 << 3,
-    StrutAreaAll     = StrutAreaTop | StrutAreaRight | StrutAreaBottom | StrutAreaLeft
+    StrutAreaTop = 1 << 0,
+    StrutAreaRight = 1 << 1,
+    StrutAreaBottom = 1 << 2,
+    StrutAreaLeft = 1 << 3,
+    StrutAreaAll = StrutAreaTop | StrutAreaRight | StrutAreaBottom | StrutAreaLeft
 };
 Q_DECLARE_FLAGS(StrutAreas, StrutArea)
 
-class KWIN_EXPORT StrutRect : public QRect
+class StrutRect : public QRect
 {
 public:
-    explicit StrutRect(QRect rect = QRect(), StrutArea area = StrutAreaInvalid);
-    StrutRect(const StrutRect& other);
-    StrutRect &operator=(const StrutRect& other);
-    inline StrutArea area() const {
+    explicit StrutRect(QRect rect = QRect(), StrutArea area = StrutAreaInvalid)
+        : QRect(rect)
+        , m_area(area)
+    {
+    }
+
+    StrutRect(StrutRect const& other)
+        : QRect(other)
+        , m_area(other.area())
+    {
+    }
+
+    StrutRect& operator=(StrutRect const& other)
+    {
+        if (this != &other) {
+            QRect::operator=(other);
+            m_area = other.area();
+        }
+        return *this;
+    }
+
+    inline StrutArea area() const
+    {
         return m_area;
     }
+
 private:
     StrutArea m_area;
 };
-typedef QVector<StrutRect> StrutRects;
 
-} // namespace
+using StrutRects = QVector<StrutRect>;
 
-// Must be outside namespace
+}
+
 Q_DECLARE_OPERATORS_FOR_FLAGS(KWin::StrutAreas)
-
-#endif
