dda42615b9da220ed25e1b52a3a6a7a86f7e86d1
Roman Gilg <subdiff@gmail.com>
2022-03-03 19:03:07 +0100

    feat(effect): install separate paint clipper header
    The implementation is moved into a separate source file too. That reduces the
    size of the kwineffects header and source files.

--- libkwineffects/kwineffects.h
+++ libkwineffects/kwineffects.h
@@ -28,6 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <kwineffects/effect_screen.h>
 #include <kwineffects/effect_window.h>
 #include <kwineffects/effects_handler.h>
+#include <kwineffects/paint_clipper.h>
 #include <kwineffects/paint_data.h>
 #include <kwineffects/types.h>
 #include <kwineffects/window_quad.h>
@@ -158,73 +159,6 @@ class XRenderPicture;
  *
  * @{
  */
-
-/**
- * @short Helper class for restricting painting area only to allowed area.
- *
- * This helper class helps specifying areas that should be painted, clipping
- * out the rest. The simplest usage is creating an object on the stack
- * and giving it the area that is allowed to be painted to. When the object
- * is destroyed, the restriction will be removed.
- * Note that all painting code must use paintArea() to actually perform the clipping.
- */
-class KWINEFFECTS_EXPORT PaintClipper
-{
-public:
-    /**
-     * Calls push().
-     */
-    explicit PaintClipper(const QRegion& allowed_area);
-    /**
-     * Calls pop().
-     */
-    ~PaintClipper();
-    /**
-     * Allows painting only in the given area. When areas have been already
-     * specified, painting is allowed only in the intersection of all areas.
-     */
-    static void push(const QRegion& allowed_area);
-    /**
-     * Removes the given area. It must match the top item in the stack.
-     */
-    static void pop(const QRegion& allowed_area);
-    /**
-     * Returns true if any clipping should be performed.
-     */
-    static bool clip();
-    /**
-     * If clip() returns true, this function gives the resulting area in which
-     * painting is allowed. It is usually simpler to use the helper Iterator class.
-     */
-    static QRegion paintArea();
-    /**
-     * Helper class to perform the clipped painting. The usage is:
-     * @code
-     * for ( PaintClipper::Iterator iterator;
-     *      !iterator.isDone();
-     *      iterator.next())
-     *     { // do the painting, possibly use iterator.boundingRect()
-     *     }
-     * @endcode
-     */
-    class KWINEFFECTS_EXPORT Iterator
-    {
-    public:
-        Iterator();
-        ~Iterator();
-        bool isDone();
-        void next();
-        QRect boundingRect() const;
-
-    private:
-        struct Data;
-        Data* data;
-    };
-
-private:
-    QRegion area;
-    static QStack<QRegion>* areas;
-};
 
 /**
  * @internal
