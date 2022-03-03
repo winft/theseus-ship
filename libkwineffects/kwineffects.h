0a6011ce5181f642132d74ab1a34cc06010340d0
Roman Gilg <subdiff@gmail.com>
2022-03-03 19:21:10 +0100

    feat(effect): install separate effect frame header
    The implementation is moved into a separate source file too. That reduces the
    size of the kwineffects header and source files.

--- libkwineffects/kwineffects.h
+++ libkwineffects/kwineffects.h
@@ -24,6 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define KWINEFFECTS_H
 
 #include <kwineffects/effect.h>
+#include <kwineffects/effect_frame.h>
 #include <kwineffects/effect_plugin_factory.h>
 #include <kwineffects/effect_screen.h>
 #include <kwineffects/effect_window.h>
@@ -64,8 +65,6 @@ class QMatrix4x4;
 namespace KWin
 {
 
-class EffectFramePrivate;
-class GLShader;
 class XRenderPicture;
 
 /** @defgroup kwineffects KWin effects library
@@ -160,135 +159,6 @@ class XRenderPicture;
  *
  * @{
  */
-
-/**
- * @short Helper class for displaying text and icons in frames.
- *
- * Paints text and/or and icon with an optional frame around them. The
- * available frames includes one that follows the default Plasma theme and
- * another that doesn't.
- * It is recommended to use this class whenever displaying text.
- */
-class KWINEFFECTS_EXPORT EffectFrame
-{
-public:
-    EffectFrame();
-    virtual ~EffectFrame();
-
-    /**
-     * Delete any existing textures to free up graphics memory. They will
-     * be automatically recreated the next time they are required.
-     */
-    virtual void free() = 0;
-
-    /**
-     * Render the frame.
-     */
-    virtual void render(const QRegion& region = infiniteRegion(),
-                        double opacity = 1.0,
-                        double frameOpacity = 1.0)
-        = 0;
-
-    virtual void setPosition(const QPoint& point) = 0;
-    /**
-     * Set the text alignment for static frames and the position alignment
-     * for non-static.
-     */
-    virtual void setAlignment(Qt::Alignment alignment) = 0;
-    virtual Qt::Alignment alignment() const = 0;
-    virtual void setGeometry(const QRect& geometry, bool force = false) = 0;
-    virtual const QRect& geometry() const = 0;
-
-    virtual void setText(const QString& text) = 0;
-    virtual const QString& text() const = 0;
-    virtual void setFont(const QFont& font) = 0;
-    virtual const QFont& font() const = 0;
-    /**
-     * Set the icon that will appear on the left-hand size of the frame.
-     */
-    virtual void setIcon(const QIcon& icon) = 0;
-    virtual const QIcon& icon() const = 0;
-    virtual void setIconSize(const QSize& size) = 0;
-    virtual const QSize& iconSize() const = 0;
-
-    /**
-     * Sets the geometry of a selection.
-     * To remove the selection set a null rect.
-     * @param selection The geometry of the selection in screen coordinates.
-     */
-    virtual void setSelection(const QRect& selection) = 0;
-
-    /**
-     * @param shader The GLShader for rendering.
-     */
-    virtual void setShader(GLShader* shader) = 0;
-    /**
-     * @returns The GLShader used for rendering or null if none.
-     */
-    virtual GLShader* shader() const = 0;
-
-    /**
-     * @returns The style of this EffectFrame.
-     */
-    virtual EffectFrameStyle style() const = 0;
-
-    /**
-     * If @p enable is @c true cross fading between icons and text is enabled
-     * By default disabled. Use setCrossFadeProgress to cross fade.
-     * Cross Fading is currently only available if OpenGL is used.
-     * @param enable @c true enables cross fading, @c false disables it again
-     * @see isCrossFade
-     * @see setCrossFadeProgress
-     * @since 4.6
-     */
-    void enableCrossFade(bool enable);
-    /**
-     * @returns @c true if cross fading is enabled, @c false otherwise
-     * @see enableCrossFade
-     * @since 4.6
-     */
-    bool isCrossFade() const;
-    /**
-     * Sets the current progress for cross fading the last used icon/text
-     * with current icon/text to @p progress.
-     * A value of 0.0 means completely old icon/text, a value of 1.0 means
-     * completely current icon/text.
-     * Default value is 1.0. You have to enable cross fade before using it.
-     * Cross Fading is currently only available if OpenGL is used.
-     * @see enableCrossFade
-     * @see isCrossFade
-     * @see crossFadeProgress
-     * @since 4.6
-     */
-    void setCrossFadeProgress(qreal progress);
-    /**
-     * @returns The current progress for cross fading
-     * @see setCrossFadeProgress
-     * @see enableCrossFade
-     * @see isCrossFade
-     * @since 4.6
-     */
-    qreal crossFadeProgress() const;
-
-    /**
-     * Returns The projection matrix as used by the current screen painting pass
-     * including screen transformations.
-     *
-     * This matrix is only valid during a rendering pass started by render.
-     *
-     * @since 5.6
-     * @see render
-     * @see EffectsHandler::paintEffectFrame
-     * @see Effect::paintEffectFrame
-     */
-    QMatrix4x4 screenProjectionMatrix() const;
-
-protected:
-    void setScreenProjectionMatrix(const QMatrix4x4& projection);
-
-private:
-    EffectFramePrivate* const d;
-};
 
 /**
  * The TimeLine class is a helper for controlling animations.
