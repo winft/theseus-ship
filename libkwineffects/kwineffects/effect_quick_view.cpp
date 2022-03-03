08228e8d0170dd089ec0e65452074cf07440d640
Roman Gilg <subdiff@gmail.com>
2022-03-03 21:07:38 +0100

    feat(effect): add effect quick view header to installed subdirectory
    We provide header and source in a kwineffects subdirectory. This corresponds to
    the install directory. We keep the original header for compatibility.

--- libkwineffects/kwineffects/effect_quick_view.cpp
+++ libkwineffects/kwineffects/effect_quick_view.cpp
@@ -1,43 +1,26 @@
-/********************************************************************
- KWin - the KDE window manager
- This file is part of the KDE project.
+/*
+    SPDX-FileCopyrightText: 2019 David Edmundson <davidedmundson@kde.org>
+    SPDX-FileCopyrightText: 2022 Roman Gilg <subdiff@gmail.com>
 
-Copyright (C) 2019 David Edmundson <davidedmundson@kde.org>
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "effect_quick_view.h"
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*********************************************************************/
-
-#include "kwineffectquickview.h"
-
-#include "kwineffects.h"
+#include "effects_handler.h"
 #include "kwinglutils.h"
 #include "logging_p.h"
 
-#include <QQmlComponent>
-#include <QQmlContext>
-#include <QQmlEngine>
-#include <QQuickItem>
-#include <QQuickRenderControl>
-#include <QQuickView>
-#include <QUrl>
-
+#include <KDeclarative/QmlObjectSharedEngine>
 #include <QOffscreenSurface>
 #include <QOpenGLContext>
 #include <QOpenGLFramebufferObject>
+#include <QPointer>
+#include <QQuickItem>
+#include <QQuickRenderControl>
+#include <QQuickWindow>
 #include <QTimer>
-
-#include <KDeclarative/QmlObjectSharedEngine>
+#include <QTouchEvent>
+#include <QWindow>
 
 namespace KWin
 {
@@ -625,4 +608,4 @@ QQuickItem* EffectQuickScene::rootItem() const
 
 }
 
-#include "kwineffectquickview.moc"
+#include "effect_quick_view.moc"
