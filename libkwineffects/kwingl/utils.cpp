0c21c3284796a2a2cc14733356645674d59afc0d
Roman Gilg <subdiff@gmail.com>
2022-03-04 12:54:17 +0100

    feat(effect): install GL utils header to subdirectory
    We provide header and source in an installed kwingl library subdirectory and
    keep the original header for compatibility.

--- libkwineffects/kwingl/utils.cpp
+++ libkwineffects/kwingl/utils.cpp
@@ -1,25 +1,11 @@
-/********************************************************************
- KWin - the KDE window manager
- This file is part of the KDE project.
+/*
+    SPDX-FileCopyrightText: 2007 Rivo Laks <rivolaks@hot.ee>
+    SPDX-FileCopyrightText: 2011 Martin Gräßlin <mgraesslin@kde.org>
+    SPDX-FileCopyrightText: 2022 Roman Gilg <subdiff@gmail.com>
 
-Copyright (C) 2006-2007 Rivo Laks <rivolaks@hot.ee>
-Copyright (C) 2010, 2011 Martin Gräßlin <mgraesslin@kde.org>
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*********************************************************************/
-
-#include "kwinglutils.h"
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "utils.h"
 
 // need to call GLTexturePrivate::initStatic()
 #include "kwingltexture_p.h"
@@ -2295,4 +2281,4 @@ GLVertexBuffer* GLVertexBuffer::streamingBuffer()
     return GLVertexBufferPrivate::streamingBuffer;
 }
 
-} // namespace
+}
