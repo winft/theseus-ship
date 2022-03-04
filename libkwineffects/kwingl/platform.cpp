b8227b3facfb77014ee63746d60f82c882f87c2a
Roman Gilg <subdiff@gmail.com>
2022-03-04 13:08:57 +0100

    feat(effect): add GL platform header to installed subdirectory
    We provide header and source in a kwingl subdirectory. This corresponds to
    the install directory. We keep the original header for compatibility.

--- libkwineffects/kwingl/platform.cpp
+++ libkwineffects/kwingl/platform.cpp
@@ -1,27 +1,15 @@
-/********************************************************************
- KWin - the KDE window manager
- This file is part of the KDE project.
+/*
+    SPDX-FileCopyrightText: 2010 Fredrik Höglund <fredrik@kde.org>
+    SPDX-FileCopyrightText: 2022 Roman Gilg <subdiff@gmail.com>
 
-Copyright (C) 2010 Fredrik Höglund <fredrik@kde.org>
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "platform.h"
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*********************************************************************/
-
-#include "kwinglplatform.h"
 // include kwinglutils_funcs.h to avoid the redeclaration issues
 // between qopengl.h and epoxy/gl.h
 #include "kwinglutils_funcs.h"
+
 #include <epoxy/gl.h>
 
 #include <QDebug>
@@ -1305,4 +1293,4 @@ void GLPlatform::cleanup()
     s_platform = nullptr;
 }
 
-} // namespace KWin
+}
