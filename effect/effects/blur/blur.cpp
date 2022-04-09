winft/theseus-ship

commit de005053cfedff195725d5aba5d808a921d79596

fix(effect): avoid shrinking unrelated opaque regions for blur
Currently, the blur effect will shrink an opaque region even if it
doesn't intersect m_currentBlur.

This ensures that the blur effect won't do a stupid thing such as
clipping the opaque region of the desktop window.

effect/effects/blur/blur.cpp
@@ -430,20 +430,22 @@ void BlurEffect::prePaintWindow(EffectWindow* w,
         return;
     }
 
-    // to blur an area partially we have to shrink the opaque area of a window
-    QRegion newClip;
-    const QRegion oldClip = data.clip;
-    for (const QRect& rect : data.clip) {
-        newClip |= rect.adjusted(m_expandSize, m_expandSize, -m_expandSize, -m_expandSize);
-    }
-    data.clip = newClip;
+    if (data.clip.intersects(m_currentBlur)) {
+        // to blur an area partially we have to shrink the opaque area of a window
+        QRegion newClip;
+        const QRegion oldClip = data.clip;
+        for (const QRect& rect : data.clip) {
+            newClip |= rect.adjusted(m_expandSize, m_expandSize, -m_expandSize, -m_expandSize);
+        }
+        data.clip = newClip;
 
-    // we don't have to blur a region we don't see
-    m_currentBlur -= newClip;
-    // if we have to paint a non-opaque part of this window that intersects with the
-    // currently blurred region we have to redraw the whole region
-    if ((data.paint - oldClip).intersects(m_currentBlur)) {
-        data.paint |= m_currentBlur;
+        // we don't have to blur a region we don't see
+        m_currentBlur -= newClip;
+        // if we have to paint a non-opaque part of this window that intersects with the
+        // currently blurred region we have to redraw the whole region
+        if ((data.paint - oldClip).intersects(m_currentBlur)) {
+            data.paint |= m_currentBlur;
+        }
     }
 
     // in case this window has regions to be blurred

commit 39cf2f0f6f4623093c4734144c5fa21977e6898e

fix(effect): remove window flickering on blur
This fixes a bug introduced in de005053c where it would no longer check if it
should redraw the whole region unless the data clip intersected. This would
lead to flickering in certain cases

BUG: 421135

effect/effects/blur/blur.cpp
@@ -430,10 +430,10 @@ void BlurEffect::prePaintWindow(EffectWindow* w,
         return;
     }
 
+    const QRegion oldClip = data.clip;
     if (data.clip.intersects(m_currentBlur)) {
         // to blur an area partially we have to shrink the opaque area of a window
         QRegion newClip;
-        const QRegion oldClip = data.clip;
         for (const QRect& rect : data.clip) {
             newClip |= rect.adjusted(m_expandSize, m_expandSize, -m_expandSize, -m_expandSize);
         }
@@ -441,11 +441,12 @@ void BlurEffect::prePaintWindow(EffectWindow* w,
 
         // we don't have to blur a region we don't see
         m_currentBlur -= newClip;
-        // if we have to paint a non-opaque part of this window that intersects with the
-        // currently blurred region we have to redraw the whole region
-        if ((data.paint - oldClip).intersects(m_currentBlur)) {
-            data.paint |= m_currentBlur;
-        }
+    }
+
+    // if we have to paint a non-opaque part of this window that intersects with the
+    // currently blurred region we have to redraw the whole region
+    if ((data.paint - oldClip).intersects(m_currentBlur)) {
+        data.paint |= m_currentBlur;
     }
 
     // in case this window has regions to be blurred