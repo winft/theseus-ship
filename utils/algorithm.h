056a1f397f85db12a88f22a10a6e493c54d8dd7c
Tom Englund <tomenglund26@gmail.com>
2022-06-21 12:15:25 +0200

    feat: add move_to util functions
    add move_to_front/move_to_back and slide util functions.
    slide takes 3 iterators, f , l , p which means it takes the iterators
    and the range between f and l and moves it to p.
    move_to_front/back tries to find one element identified by arg and slide it to front
    or end, returns if such an element was found.

--- utils/algorithm.h
+++ utils/algorithm.h
@@ -38,6 +38,51 @@ bool contains_if(V const& container, F&& f)
     return std::find_if(container.cbegin(), container.cend(), f) != container.cend();
 }
 
+/// takes the range between iterator f and l and moves it to p, returns the range it ended up in.
+template<typename It>
+auto slide(It f, It l, It p) -> std::pair<It, It>
+{
+    if (p < f) {
+        return {p, std::rotate(p, f, l)};
+    }
+    if (l < p) {
+        return {std::rotate(f, l, p), p};
+    }
+    return {f, l};
+}
+
+/// Moves the first element identified by arg to the back. Returns if such an element was found.
+template<typename V, typename T>
+bool move_to_back(V& container, T const& arg)
+{
+    auto it = find(container, arg);
+
+    if (it == container.end()) {
+        return false;
+    }
+
+    auto one_item = it;
+    std::advance(one_item, 1);
+    slide(it, one_item, container.end());
+    return true;
+}
+
+/// Moves the first element identified by arg to the front. Returns if such an element was found.
+template<typename V, typename T>
+bool move_to_front(V& container, T const& arg)
+{
+    auto it = find(container, arg);
+
+    if (it == container.end()) {
+        return false;
+    }
+
+    auto one_item = it;
+    std::advance(one_item, 1);
+    slide(it, one_item, container.begin());
+    return true;
+}
+
 template<typename V, typename T>
 void remove_all(V& container, T const& arg)
 {
