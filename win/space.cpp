db1c189505f1bd727c6859376d4ad3039e8abc6a
Roman Gilg <subdiff@gmail.com>
2022-07-06 12:30:02 +0200

    refactor: replace shortcut macros with lambdas
    We have to use some QStrings instead of QStringLiterals, which is less
    performant. But we may change to std::string later on anyway.

--- win/space.cpp
+++ win/space.cpp
@@ -1465,28 +1465,33 @@ void space::init_shortcut_with_action_arg(const QString& actionName,
 void space::initShortcuts()
 {
     // Some shortcuts have Tarzan-speech like names, they need extra
-    // normal human descriptions with DEF2() the others can use DEF()
-    // new DEF3 allows to pass data to the action, replacing the %1 argument in the name
+    // normal human descriptions with def2() the others can use def()
+    // new def3 allows to pass data to the action, replacing the %1 argument in the name
 
-#define DEF2(name, descr, key, fnSlot)                                                             \
-    initShortcut(QStringLiteral(name), descr.toString(), key, [this] { fnSlot(*this); });
+    auto def2 = [this](auto name, auto descr, auto key, auto functor) {
+        initShortcut(QString(name), descr.toString(), key, [this, functor] { functor(*this); });
+    };
 
-#define DEF(name, key, fnSlot)                                                                     \
-    initShortcut(QString::fromUtf8(name.untranslatedText()), name.toString(), key, [this] {        \
-        fnSlot(*this);                                                                             \
-    });
+    auto def = [this](auto name, auto key, auto functor) {
+        initShortcut(QString::fromUtf8(name.untranslatedText()),
+                     name.toString(),
+                     key,
+                     [this, functor] { functor(*this); });
+    };
 
-#define DEF3(name, key, fnSlot, value)                                                             \
-    init_shortcut_with_action_arg(                                                                 \
-        QString::fromUtf8(name.untranslatedText()).arg(value),                                     \
-        name.subs(value).toString(),                                                               \
-        key,                                                                                       \
-        qobject.get(),                                                                             \
-        [this](QAction* action) { fnSlot(*this, action); },                                        \
-        value);
+    auto def3 = [this](auto name, auto key, auto functor, auto value) {
+        init_shortcut_with_action_arg(
+            QString::fromUtf8(name.untranslatedText()).arg(value),
+            name.subs(value).toString(),
+            key,
+            qobject.get(),
+            [this, functor](QAction* action) { functor(*this, action); },
+            value);
+    };
 
-#define DEF4(name, descr, key, functor)                                                            \
-    initShortcut(QStringLiteral(name), descr.toString(), key, functor);
+    auto def4 = [this](auto name, auto descr, auto key, auto functor) {
+        initShortcut(QString(name), descr.toString(), key, functor);
+    };
 
     auto def5 = [this](auto name, auto key, auto functor, auto value) {
         initShortcut(QString::fromUtf8(name.untranslatedText()).arg(value),
@@ -1496,134 +1501,142 @@ void space::initShortcuts()
                      value);
     };
 
-#define DEF6(name, key, target, fnSlot)                                                            \
-    initShortcut(QString::fromUtf8(name.untranslatedText()), name.toString(), key, target, &fnSlot);
+    auto def6 = [this](auto name, auto key, auto target, auto functor) {
+        initShortcut(
+            QString::fromUtf8(name.untranslatedText()), name.toString(), key, target, functor);
+    };
 
-    DEF(kli18n("Window Operations Menu"),
+    def(kli18n("Window Operations Menu"),
         Qt::ALT + Qt::Key_F3,
-        active_window_show_operations_popup);
-    DEF2("Window Close", kli18n("Close Window"), Qt::ALT + Qt::Key_F4, active_window_close);
-    DEF2("Window Maximize",
+        active_window_show_operations_popup<space>);
+    def2("Window Close", kli18n("Close Window"), Qt::ALT + Qt::Key_F4, active_window_close<space>);
+    def2("Window Maximize",
          kli18n("Maximize Window"),
          Qt::META + Qt::Key_PageUp,
-         active_window_maximize);
-    DEF2("Window Maximize Vertical",
+         active_window_maximize<space>);
+    def2("Window Maximize Vertical",
          kli18n("Maximize Window Vertically"),
          0,
-         active_window_maximize_vertical);
-    DEF2("Window Maximize Horizontal",
+         active_window_maximize_vertical<space>);
+    def2("Window Maximize Horizontal",
          kli18n("Maximize Window Horizontally"),
          0,
-         active_window_maximize_horizontal);
-    DEF2("Window Minimize",
+         active_window_maximize_horizontal<space>);
+    def2("Window Minimize",
          kli18n("Minimize Window"),
          Qt::META + Qt::Key_PageDown,
-         active_window_minimize);
-    DEF2("Window Move", kli18n("Move Window"), 0, active_window_move);
-    DEF2("Window Resize", kli18n("Resize Window"), 0, active_window_resize);
-    DEF2("Window Raise", kli18n("Raise Window"), 0, active_window_raise);
-    DEF2("Window Lower", kli18n("Lower Window"), 0, active_window_lower);
-    DEF(kli18n("Toggle Window Raise/Lower"), 0, active_window_raise_or_lower);
-    DEF2("Window Fullscreen", kli18n("Make Window Fullscreen"), 0, active_window_set_fullscreen);
-    DEF2("Window No Border", kli18n("Hide Window Border"), 0, active_window_set_no_border);
-    DEF2("Window Above Other Windows",
+         active_window_minimize<space>);
+    def2("Window Move", kli18n("Move Window"), 0, active_window_move<space>);
+    def2("Window Resize", kli18n("Resize Window"), 0, active_window_resize<space>);
+    def2("Window Raise", kli18n("Raise Window"), 0, active_window_raise<space>);
+    def2("Window Lower", kli18n("Lower Window"), 0, active_window_lower<space>);
+    def(kli18n("Toggle Window Raise/Lower"), 0, active_window_raise_or_lower<space>);
+    def2("Window Fullscreen",
+         kli18n("Make Window Fullscreen"),
+         0,
+         active_window_set_fullscreen<space>);
+    def2("Window No Border", kli18n("Hide Window Border"), 0, active_window_set_no_border<space>);
+    def2("Window Above Other Windows",
          kli18n("Keep Window Above Others"),
          0,
-         active_window_set_keep_above);
-    DEF2("Window Below Other Windows",
+         active_window_set_keep_above<space>);
+    def2("Window Below Other Windows",
          kli18n("Keep Window Below Others"),
          0,
-         active_window_set_keep_below);
-    DEF(kli18n("Activate Window Demanding Attention"),
+         active_window_set_keep_below<space>);
+    def(kli18n("Activate Window Demanding Attention"),
         Qt::META | Qt::CTRL | Qt::Key_A,
-        activate_attention_window);
-    DEF(kli18n("Setup Window Shortcut"), 0, active_window_setup_window_shortcut);
-    DEF2("Window Pack Right", kli18n("Pack Window to the Right"), 0, active_window_pack_right);
-    DEF2("Window Pack Left", kli18n("Pack Window to the Left"), 0, active_window_pack_left);
-    DEF2("Window Pack Up", kli18n("Pack Window Up"), 0, active_window_pack_up);
-    DEF2("Window Pack Down", kli18n("Pack Window Down"), 0, active_window_pack_down);
-    DEF2("Window Grow Horizontal",
+        activate_attention_window<space>);
+    def(kli18n("Setup Window Shortcut"), 0, active_window_setup_window_shortcut<space>);
+    def2("Window Pack Right",
+         kli18n("Pack Window to the Right"),
+         0,
+         active_window_pack_right<space>);
+    def2("Window Pack Left", kli18n("Pack Window to the Left"), 0, active_window_pack_left<space>);
+    def2("Window Pack Up", kli18n("Pack Window Up"), 0, active_window_pack_up<space>);
+    def2("Window Pack Down", kli18n("Pack Window Down"), 0, active_window_pack_down<space>);
+    def2("Window Grow Horizontal",
          kli18n("Pack Grow Window Horizontally"),
          0,
-         active_window_grow_horizontal);
-    DEF2("Window Grow Vertical",
+         active_window_grow_horizontal<space>);
+    def2("Window Grow Vertical",
          kli18n("Pack Grow Window Vertically"),
          0,
-         active_window_grow_vertical);
-    DEF2("Window Shrink Horizontal",
+         active_window_grow_vertical<space>);
+    def2("Window Shrink Horizontal",
          kli18n("Pack Shrink Window Horizontally"),
          0,
-         active_window_shrink_horizontal);
-    DEF2("Window Shrink Vertical",
+         active_window_shrink_horizontal<space>);
+    def2("Window Shrink Vertical",
          kli18n("Pack Shrink Window Vertically"),
          0,
-         active_window_shrink_vertical);
-    DEF4("Window Quick Tile Left",
+         active_window_shrink_vertical<space>);
+    def4("Window Quick Tile Left",
          kli18n("Quick Tile Window to the Left"),
          Qt::META + Qt::Key_Left,
          std::bind(&space::quickTileWindow, this, win::quicktiles::left));
-    DEF4("Window Quick Tile Right",
+    def4("Window Quick Tile Right",
          kli18n("Quick Tile Window to the Right"),
          Qt::META + Qt::Key_Right,
          std::bind(&space::quickTileWindow, this, win::quicktiles::right));
-    DEF4("Window Quick Tile Top",
+    def4("Window Quick Tile Top",
          kli18n("Quick Tile Window to the Top"),
          Qt::META + Qt::Key_Up,
          std::bind(&space::quickTileWindow, this, win::quicktiles::top));
-    DEF4("Window Quick Tile Bottom",
+    def4("Window Quick Tile Bottom",
          kli18n("Quick Tile Window to the Bottom"),
          Qt::META + Qt::Key_Down,
          std::bind(&space::quickTileWindow, this, win::quicktiles::bottom));
-    DEF4("Window Quick Tile Top Left",
+    def4("Window Quick Tile Top Left",
          kli18n("Quick Tile Window to the Top Left"),
          0,
          std::bind(&space::quickTileWindow, this, win::quicktiles::top | win::quicktiles::left));
-    DEF4("Window Quick Tile Bottom Left",
+    def4("Window Quick Tile Bottom Left",
          kli18n("Quick Tile Window to the Bottom Left"),
          0,
          std::bind(&space::quickTileWindow, this, win::quicktiles::bottom | win::quicktiles::left));
-    DEF4("Window Quick Tile Top Right",
+    def4("Window Quick Tile Top Right",
          kli18n("Quick Tile Window to the Top Right"),
          0,
          std::bind(&space::quickTileWindow, this, win::quicktiles::top | win::quicktiles::right));
-    DEF4(
+    def4(
         "Window Quick Tile Bottom Right",
         kli18n("Quick Tile Window to the Bottom Right"),
         0,
         std::bind(&space::quickTileWindow, this, win::quicktiles::bottom | win::quicktiles::right));
-    DEF4("Switch Window Up",
+    def4("Switch Window Up",
          kli18n("Switch to Window Above"),
          Qt::META + Qt::ALT + Qt::Key_Up,
          std::bind(
              static_cast<void (space::*)(Direction)>(&space::switchWindow), this, DirectionNorth));
-    DEF4("Switch Window Down",
+    def4("Switch Window Down",
          kli18n("Switch to Window Below"),
          Qt::META + Qt::ALT + Qt::Key_Down,
          std::bind(
              static_cast<void (space::*)(Direction)>(&space::switchWindow), this, DirectionSouth));
-    DEF4("Switch Window Right",
+    def4("Switch Window Right",
          kli18n("Switch to Window to the Right"),
          Qt::META + Qt::ALT + Qt::Key_Right,
          std::bind(
              static_cast<void (space::*)(Direction)>(&space::switchWindow), this, DirectionEast));
-    DEF4("Switch Window Left",
+    def4("Switch Window Left",
          kli18n("Switch to Window to the Left"),
          Qt::META + Qt::ALT + Qt::Key_Left,
          std::bind(
              static_cast<void (space::*)(Direction)>(&space::switchWindow), this, DirectionWest));
-    DEF2("Increase Opacity",
+    def2("Increase Opacity",
          kli18n("Increase Opacity of Active Window by 5 %"),
          0,
-         active_window_increase_opacity);
-    DEF2("Decrease Opacity",
+         active_window_increase_opacity<space>);
+    def2("Decrease Opacity",
          kli18n("Decrease Opacity of Active Window by 5 %"),
          0,
-         active_window_lower_opacity);
+         active_window_lower_opacity<space>);
 
-    DEF2("Window On All Desktops",
+    def2("Window On All Desktops",
          kli18n("Keep Window on All Desktops"),
          0,
-         active_window_set_on_all_desktops);
+         active_window_set_on_all_desktops<space>);
 
     for (int i = 1; i < 21; ++i) {
         def5(
@@ -1633,43 +1646,35 @@ void space::initShortcuts()
             i);
     }
 
-    DEF(kli18n("Window to Next Desktop"), 0, active_window_to_next_desktop);
-    DEF(kli18n("Window to Previous Desktop"), 0, active_window_to_prev_desktop);
-    DEF(kli18n("Window One Desktop to the Right"), 0, active_window_to_right_desktop);
-    DEF(kli18n("Window One Desktop to the Left"), 0, active_window_to_left_desktop);
-    DEF(kli18n("Window One Desktop Up"), 0, active_window_to_above_desktop);
-    DEF(kli18n("Window One Desktop Down"), 0, active_window_to_below_desktop);
+    def(kli18n("Window to Next Desktop"), 0, active_window_to_next_desktop<space>);
+    def(kli18n("Window to Previous Desktop"), 0, active_window_to_prev_desktop<space>);
+    def(kli18n("Window One Desktop to the Right"), 0, active_window_to_right_desktop<space>);
+    def(kli18n("Window One Desktop to the Left"), 0, active_window_to_left_desktop<space>);
+    def(kli18n("Window One Desktop Up"), 0, active_window_to_above_desktop<space>);
+    def(kli18n("Window One Desktop Down"), 0, active_window_to_below_desktop<space>);
 
     for (int i = 0; i < 8; ++i) {
-        DEF3(kli18n("Window to Screen %1"), 0, active_window_to_output, i);
+        def3(kli18n("Window to Screen %1"), 0, active_window_to_output<space>, i);
     }
-    DEF(kli18n("Window to Next Screen"), 0, active_window_to_next_output);
-    DEF(kli18n("Window to Previous Screen"), 0, active_window_to_prev_output);
-    DEF(kli18n("Show Desktop"), Qt::META + Qt::Key_D, toggle_show_desktop);
+    def(kli18n("Window to Next Screen"), 0, active_window_to_next_output<space>);
+    def(kli18n("Window to Previous Screen"), 0, active_window_to_prev_output<space>);
+    def(kli18n("Show Desktop"), Qt::META + Qt::Key_D, toggle_show_desktop<space>);
 
     for (int i = 0; i < 8; ++i) {
-        DEF3(kli18n("Switch to Screen %1"), 0, switch_to_output, i);
+        def3(kli18n("Switch to Screen %1"), 0, switch_to_output<space>, i);
     }
 
-    DEF(kli18n("Switch to Next Screen"), 0, switch_to_next_output);
-    DEF(kli18n("Switch to Previous Screen"), 0, switch_to_prev_output);
+    def(kli18n("Switch to Next Screen"), 0, switch_to_next_output<space>);
+    def(kli18n("Switch to Previous Screen"), 0, switch_to_prev_output<space>);
 
-    DEF(kli18n("Kill Window"), Qt::META | Qt::CTRL | Qt::Key_Escape, start_window_killer);
-    DEF6(kli18n("Suspend Compositing"),
+    def(kli18n("Kill Window"), Qt::META | Qt::CTRL | Qt::Key_Escape, start_window_killer<space>);
+    def6(kli18n("Suspend Compositing"),
          Qt::SHIFT + Qt::ALT + Qt::Key_F12,
          &render,
-         render::compositor::toggleCompositing);
-    DEF6(kli18n("Invert Screen Colors"),
-         0,
-         kwinApp()->get_base().render.get(),
-         render::platform::invertScreen);
-
-#undef DEF
-#undef DEF2
-#undef DEF3
-#undef DEF4
-#undef DEF5
-#undef DEF6
+         [render = &render] { render->toggleCompositing(); });
+    def6(kli18n("Invert Screen Colors"), 0, &render, [render = &render] {
+        render->platform.invertScreen();
+    });
 
 #if KWIN_BUILD_TABBOX
     tabbox->init_shortcuts();
