d843e9279cf12eb357479824e48418b94715e302
Roman Gilg <subdiff@gmail.com>
2022-07-27 03:28:26 +0200

    refactor(input): define filter helpers in header
    This allows to afterwards template the functions.

--- input/filters/helpers.h
+++ input/filters/helpers.h
@@ -5,13 +5,25 @@
 */
 #pragma once
 
+#include "base/options.h"
+#include "base/wayland/server.h"
 #include "input/event.h"
+#include "input/keyboard_redirect.h"
+#include "input/pointer_redirect.h"
+#include "input/qt_event.h"
+#include "input/redirect.h"
+#include "input/xkb/helpers.h"
+#include "main.h"
+#include "win/input.h"
+#include "win/space.h"
+
+#include <Wrapland/Server/keyboard_pool.h>
+#include <Wrapland/Server/seat.h>
 
 #include <QWheelEvent>
 
 namespace KWin
 {
-class Toplevel;
 
 namespace input
 {
@@ -21,14 +33,117 @@ enum class MouseAction {
     ModifierAndWindow,
 };
 
-std::pair<bool, bool> perform_mouse_modifier_action(button_event const& event, Toplevel* window);
-std::pair<bool, bool> perform_mouse_modifier_and_window_action(button_event const& event,
-                                                               Toplevel* window);
+inline bool get_modifier_command(uint32_t key, base::options::MouseCommand& command)
+{
+    if (xkb::get_active_keyboard_modifiers_relevant_for_global_shortcuts(*kwinApp()->input)
+        != kwinApp()->options->commandAllModifier()) {
+        return false;
+    }
+    if (kwinApp()->input->redirect->get_pointer()->isConstrained()) {
+        return false;
+    }
+    if (kwinApp()->input->redirect->space.global_shortcuts_disabled) {
+        return false;
+    }
+    auto qt_key = button_to_qt_mouse_button(key);
+    switch (qt_key) {
+    case Qt::LeftButton:
+        command = kwinApp()->options->commandAll1();
+        break;
+    case Qt::MiddleButton:
+        command = kwinApp()->options->commandAll2();
+        break;
+    case Qt::RightButton:
+        command = kwinApp()->options->commandAll3();
+        break;
+    default:
+        // nothing
+        break;
+    }
+    return true;
+}
 
-std::pair<bool, bool> perform_wheel_action(axis_event const& event, Toplevel* window);
-std::pair<bool, bool> perform_wheel_and_window_action(axis_event const& event, Toplevel* window);
+inline std::pair<bool, bool> do_perform_mouse_action(base::options::MouseCommand command,
+                                                     Toplevel* window)
+{
+    return std::make_pair(true,
+                          !window->performMouseCommand(
+                              command, kwinApp()->input->redirect->get_pointer()->pos().toPoint()));
+}
 
-void pass_to_wayland_server(key_event const& event);
+inline std::pair<bool, bool> perform_mouse_modifier_action(button_event const& event,
+                                                           Toplevel* window)
+{
+    auto command = base::options::MouseNothing;
+    auto was_action = get_modifier_command(event.key, command);
+
+    return was_action ? do_perform_mouse_action(command, window) : std::make_pair(false, false);
+}
+
+inline std::pair<bool, bool> perform_mouse_modifier_and_window_action(button_event const& event,
+                                                                      Toplevel* window)
+{
+    auto command = base::options::MouseNothing;
+    auto was_action = get_modifier_command(event.key, command);
+
+    if (!was_action) {
+        command = win::get_mouse_command(window, button_to_qt_mouse_button(event.key), &was_action);
+    }
+
+    return was_action ? do_perform_mouse_action(command, window) : std::make_pair(false, false);
+}
+
+inline bool get_wheel_modifier_command(axis_orientation orientation,
+                                       double delta,
+                                       base::options::MouseCommand& command)
+{
+    if (xkb::get_active_keyboard_modifiers_relevant_for_global_shortcuts(*kwinApp()->input)
+        != kwinApp()->options->commandAllModifier()) {
+        return false;
+    }
+    if (kwinApp()->input->redirect->get_pointer()->isConstrained()) {
+        return false;
+    }
+    if (kwinApp()->input->redirect->space.global_shortcuts_disabled) {
+        return false;
+    }
+
+    auto veritcal_delta = (orientation == axis_orientation::vertical) ? -1 * delta : 0;
+    command = kwinApp()->options->operationWindowMouseWheel(veritcal_delta);
+
+    return true;
+}
+
+inline std::pair<bool, bool> perform_wheel_action(axis_event const& event, Toplevel* window)
+{
+    auto command = base::options::MouseNothing;
+    auto was_action = get_wheel_modifier_command(event.orientation, event.delta, command);
+
+    return was_action ? do_perform_mouse_action(command, window) : std::make_pair(false, false);
+}
+
+inline std::pair<bool, bool> perform_wheel_and_window_action(axis_event const& event,
+                                                             Toplevel* window)
+{
+    auto command = base::options::MouseNothing;
+    auto was_action = get_wheel_modifier_command(event.orientation, event.delta, command);
+
+    if (!was_action) {
+        command = win::get_wheel_command(window, Qt::Vertical, &was_action);
+    }
+
+    return was_action ? do_perform_mouse_action(command, window) : std::make_pair(false, false);
+}
+
+inline void pass_to_wayland_server(key_event const& event)
+{
+    auto seat = waylandServer()->seat();
+    seat->keyboards().set_keymap(event.base.dev->xkb->keymap->cache);
+    seat->keyboards().key(event.keycode,
+                          event.state == key_state::pressed
+                              ? Wrapland::Server::key_state::pressed
+                              : Wrapland::Server::key_state::released);
+}
 
 }
 }
