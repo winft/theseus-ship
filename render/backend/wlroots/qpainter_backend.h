4505e4f6283ead5c55f75ea65de6a5e0a3bf0352
Roman Gilg <subdiff@gmail.com>
2022-08-01 03:52:50 +0200

    refactor: define QPainter backend in header
    This allows templating over the platform.

--- render/backend/wlroots/qpainter_backend.h
+++ render/backend/wlroots/qpainter_backend.h
@@ -5,26 +5,67 @@
 */
 #pragma once
 
+#include "output.h"
+#include "platform.h"
+#include "qpainter_output.h"
+#include "wlr_includes.h"
+
 #include "render/qpainter/backend.h"
+#include "wayland_logging.h"
 
 namespace KWin::render::backend::wlroots
 {
 
-class platform;
+static std::unique_ptr<qpainter_output>& get_qpainter_output(base::output& output)
+{
+    auto&& wayland_output = static_cast<base::wayland::output&&>(output);
+    auto& backend_output = static_cast<wlroots::output&>(*wayland_output.render);
+    return backend_output.qpainter;
+}
 
 class qpainter_backend : public qpainter::backend
 {
 public:
-    qpainter_backend(wlroots::platform& platform);
-    ~qpainter_backend() override;
+    qpainter_backend(wlroots::platform& platform)
+        : qpainter::backend()
+        , platform{platform}
+    {
+        for (auto& out : platform.base.all_outputs) {
+            auto render
+                = static_cast<output*>(static_cast<base::wayland::output*>(out)->render.get());
+            get_qpainter_output(*out) = std::make_unique<qpainter_output>(*render, *this);
+        }
+    }
 
-    void begin_render(base::output& output) override;
-    void present(base::output* output, QRegion const& damage) override;
+    ~qpainter_backend() override
+    {
+        tear_down();
+    }
 
-    QImage* bufferForScreen(base::output* output) override;
+    void begin_render(base::output& output) override
+    {
+        get_qpainter_output(output)->begin_render();
+    }
 
-    bool needsFullRepaint() const override;
-    void tear_down();
+    void present(base::output* output, QRegion const& damage) override
+    {
+        wlr_renderer_end(platform.renderer);
+        get_qpainter_output(*output)->present(damage);
+    }
+
+    QImage* bufferForScreen(base::output* output) override
+    {
+        return get_qpainter_output(*output)->buffer.get();
+    }
+
+    bool needsFullRepaint() const override
+    {
+        return false;
+    }
+
+    void tear_down()
+    {
+    }
 
     wlroots::platform& platform;
 };
