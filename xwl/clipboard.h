9b4d1d3e761e2d8cfa4f6c09643857321736ff42
Roman Gilg <subdiff@gmail.com>
2022-08-13 01:51:25 +0200

    refactor(xwl): define selections in headers
    Prepare templating the clipboard and primary selection classes by defining them
    in header only.

--- xwl/clipboard.h
+++ xwl/clipboard.h
@@ -19,38 +19,67 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************/
 #pragma once
 
+#include "event_x11.h"
 #include "selection_data.h"
+#include "selection_wl.h"
+#include "selection_x11.h"
+#include "sources_ext.h"
+
+#include "base/wayland/server.h"
 
 #include <Wrapland/Server/data_source.h>
-
-struct xcb_xfixes_selection_notify_event_t;
+#include <Wrapland/Server/seat.h>
 
 namespace KWin::xwl
 {
-class clipboard;
-class data_source_ext;
 
 /**
  * Represents the X clipboard, which is on Wayland side just called
  * @e selection.
  */
-class KWIN_EXPORT clipboard
+class clipboard
 {
 public:
     selection_data<Wrapland::Server::data_source, data_source_ext> data;
 
-    clipboard(runtime const& core);
+    clipboard(runtime const& core)
+    {
+        data = create_selection_data<Wrapland::Server::data_source, data_source_ext>(
+            core.space->atoms->clipboard, core);
 
-    Wrapland::Server::data_source* get_current_source() const;
-    void set_selection(Wrapland::Server::data_source* source) const;
+        register_x11_selection(this, QSize(10, 10));
+
+        QObject::connect(waylandServer()->seat(),
+                         &Wrapland::Server::Seat::selectionChanged,
+                         data.qobject.get(),
+                         [this] { handle_wl_selection_change(this); });
+    }
+
+    Wrapland::Server::data_source* get_current_source() const
+    {
+        return waylandServer()->seat()->selection();
+    }
+
+    void set_selection(Wrapland::Server::data_source* source) const
+    {
+        waylandServer()->seat()->setSelection(source);
+    }
 
     void handle_x11_offer_change(std::vector<std::string> const& added,
-                                 std::vector<std::string> const& removed);
-    bool handle_client_message(xcb_client_message_event_t* event);
-    void do_handle_xfixes_notify(xcb_xfixes_selection_notify_event_t* event);
+                                 std::vector<std::string> const& removed)
+    {
+        xwl::handle_x11_offer_change(this, added, removed);
+    }
 
-private:
-    Q_DISABLE_COPY(clipboard)
+    bool handle_client_message(xcb_client_message_event_t* /*event*/)
+    {
+        return false;
+    }
+
+    void do_handle_xfixes_notify(xcb_xfixes_selection_notify_event_t* event)
+    {
+        xwl::do_handle_xfixes_notify(this, event);
+    }
 };
 
 }
--- xwl/primary_selection.h
+++ xwl/primary_selection.h
@@ -5,33 +5,64 @@
 */
 #pragma once
 
+#include "event_x11.h"
 #include "selection_data.h"
+#include "selection_wl.h"
+#include "selection_x11.h"
+#include "sources_ext.h"
+
+#include "base/wayland/server.h"
 
 #include <Wrapland/Server/primary_selection.h>
-
-struct xcb_xfixes_selection_notify_event_t;
+#include <Wrapland/Server/seat.h>
 
 namespace KWin::xwl
 {
-class primary_selection_source_ext;
 
-class KWIN_EXPORT primary_selection
+class primary_selection
 {
 public:
     selection_data<Wrapland::Server::primary_selection_source, primary_selection_source_ext> data;
 
-    primary_selection(runtime const& core);
+    primary_selection(runtime const& core)
+    {
+        data = create_selection_data<Wrapland::Server::primary_selection_source,
+                                     primary_selection_source_ext>(
+            core.x11.atoms->primary_selection, core);
 
-    Wrapland::Server::primary_selection_source* get_current_source() const;
-    void set_selection(Wrapland::Server::primary_selection_source* source) const;
+        register_x11_selection(this, QSize(10, 10));
+
+        QObject::connect(waylandServer()->seat(),
+                         &Wrapland::Server::Seat::primarySelectionChanged,
+                         data.qobject.get(),
+                         [this] { handle_wl_selection_change(this); });
+    }
+
+    Wrapland::Server::primary_selection_source* get_current_source() const
+    {
+        return waylandServer()->seat()->primarySelection();
+    }
+
+    void set_selection(Wrapland::Server::primary_selection_source* source) const
+    {
+        waylandServer()->seat()->setPrimarySelection(source);
+    }
 
     void handle_x11_offer_change(std::vector<std::string> const& added,
-                                 std::vector<std::string> const& removed);
-    bool handle_client_message(xcb_client_message_event_t* event);
-    void do_handle_xfixes_notify(xcb_xfixes_selection_notify_event_t* event);
+                                 std::vector<std::string> const& removed)
+    {
+        xwl::handle_x11_offer_change(this, added, removed);
+    }
 
-private:
-    Q_DISABLE_COPY(primary_selection)
+    bool handle_client_message(xcb_client_message_event_t* /*event*/)
+    {
+        return false;
+    }
+
+    void do_handle_xfixes_notify(xcb_xfixes_selection_notify_event_t* event)
+    {
+        xwl::do_handle_xfixes_notify(this, event);
+    }
 };
 
 }
