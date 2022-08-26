531d65490a9f44416dfa394641acae98f18dc75d
Nate Graham <nate@kde.org>
2022-08-26 12:53:33 +0200

    feat(effect): add Meta+plus as a default shortcut for Zoom effect
    Right now the Zoom effect's "zoom in" action is bound by Meta+Equals,
    which makes sense for ANSI US keyboards without numberpads which make
    you hit the Shift key before you can get a plus sign, and pressing it
    without Shift gets you an Equals sign.
    
    But some ANSI US keyboards have a numberpad with a Plus key, and many
    other keyboards without numberpads also have a Plus key, so we should
    allow users of those keyboards to trigger "zoom in" as easily as users
    of ANSI US keyboards.

--- effect/effects/zoom/zoom_config.cpp
+++ effect/effects/zoom/zoom_config.cpp
@@ -72,10 +72,9 @@ ZoomEffectConfig::ZoomEffectConfig(QWidget* parent, const QVariantList& args)
     QAction* a;
     a = actionCollection->addAction(KStandardAction::ZoomIn);
     a->setProperty("isConfigurationAction", true);
-    KGlobalAccel::self()->setDefaultShortcut(
-        a, QList<QKeySequence>() << static_cast<Qt::Key>(Qt::META) + Qt::Key_Equal);
+    KGlobalAccel::self()->setDefaultShortcut(a, QList<QKeySequence>() << (Qt::META | Qt::Key_Plus));
     KGlobalAccel::self()->setShortcut(
-        a, QList<QKeySequence>() << static_cast<Qt::Key>(Qt::META) + Qt::Key_Equal);
+        a, QList<QKeySequence>() << (Qt::META | Qt::Key_Plus) << (Qt::META | Qt::Key_Equal));
 
     a = actionCollection->addAction(KStandardAction::ZoomOut);
     a->setProperty("isConfigurationAction", true);
