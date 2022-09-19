fb0219fadb15436554561e977499056a36466b98
Eric Edlund <ericedlund2017@gmail.com>
2022-09-19 07:00:42 +0000

    fix(effect): better handle windows on all desktops in slide effect
    Change the render cycle for slide effect. Now, windows will be drawn on each
    desktop or not in the paintWindow() loop instead of the paintScreen() loop.
    This allows us to respect stacking order better by painting all of the same
    window together instead of per VD.
    
    BUG: 457486
    BUG: 458622

--- effect/effects/slide/slide.h
+++ effect/effects/slide/slide.h
@@ -44,8 +44,7 @@ namespace KWin
  * desktopChanged() is called. The desktopChanged() function configures the m_startPos and m_endPos
  * for the animation, and the duration.
  *
- * m_currentPosition and m_paintCtx.translation and everything else not labeled "drawCoordinate"
- * uses desktops as a unit.
+ * m_currentPosition and everything else not labeled "drawCoordinate" uses desktops as a unit.
  * Exmp: 1.2 means the dekstop at index 1 shifted over by .2 desktops.
  * All coords must be positive.
  *
@@ -103,7 +102,8 @@ private Q_SLOTS:
 private:
     QPoint getDrawCoords(QPointF pos, EffectScreen* screen);
     bool isTranslated(const EffectWindow* w) const;
-    bool isPainted(const EffectWindow* w) const;
+    bool isPainted(int desktopId, const EffectWindow* w) const;
+    bool willBePainted(const EffectWindow* w) const;
     bool shouldElevate(const EffectWindow* w) const;
     QPointF moveInsideDesktopGrid(QPointF p);
     QPointF constrainToDrawableRange(QPointF p);
@@ -141,12 +141,7 @@ private:
         m_currentPosition; // Should always be kept up to date with where on the grid we're seeing.
 
     struct {
-        int desktop;
-        bool firstPass;
-        bool lastPass;
-        QPointF translation; // Uses desktops as units
-
-        QPoint currentPos;
+        bool wrap;
         QVector<int> visibleDesktops;
         EffectWindowList fullscreenWindows;
     } m_paintCtx;
