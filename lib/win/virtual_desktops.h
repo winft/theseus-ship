e14bc4dd317b79d2ad77787f4a96bf9110f18197
Roman Gilg <subdiff@gmail.com>
2023-10-03 15:53:52 +0200

    docs: remove virtual desktops in code comments
    They often don't explain valuable things and make the API difficult to read and
    refactor. Therefore we remove most of the comments.

--- lib/win/virtual_desktops.h
+++ lib/win/virtual_desktops.h
@@ -79,14 +79,6 @@ private:
 
 class virtual_desktop_manager;
 
-/**
- * @brief Two dimensional grid containing the ID of the virtual desktop at a specific position
- * in the grid.
- *
- * The virtual_desktop_grid represents a visual layout of the Virtual Desktops as they are in e.g.
- * a Pager. This grid is used for getting a desktop next to a given desktop in any direction by
- * making use of the layout information. This allows navigation like move to desktop on left.
- */
 class KWIN_EXPORT virtual_desktop_grid
 {
 public:
@@ -97,19 +89,9 @@ public:
                 Qt::Orientation orientation,
                 QVector<virtual_desktop*> const& desktops);
 
-    /**
-     * @returns The coords of desktop @a id in grid units.
-     */
     QPoint gridCoords(uint id) const;
-    /**
-     * @returns The coords of desktop @a vd in grid units.
-     */
     QPoint gridCoords(virtual_desktop* vd) const;
 
-    /**
-     * @returns The desktop at the point @a coords or 0 if no desktop exists at that
-     * point. @a coords is to be in grid units.
-     */
     virtual_desktop* at(const QPoint& coords) const;
     int width() const;
     int height() const;
@@ -128,334 +110,106 @@ public:
     virtual_desktop_manager_qobject();
 
 Q_SIGNALS:
-    /**
-     * Signal emitted whenever the number of virtual desktops changes.
-     * @param previousCount The number of desktops prior to the change
-     * @param newCount The new current number of desktops
-     */
     void countChanged(uint previousCount, uint newCount);
-
-    /**
-     * Signal when the number of rows in the layout changes
-     * @param rows number of rows
-     */
     void rowsChanged(uint rows);
 
-    /**
-     * A new desktop has been created
-     * @param desktop the new just crated desktop
-     */
     void desktopCreated(KWin::win::virtual_desktop* desktop);
-
-    /**
-     * A desktop has been removed and is about to be deleted
-     * @param desktop the desktop that has been removed.
-     *          It's guaranteed to stil la valid pointer when the signal arrives,
-     *          but it's about to be deleted.
-     */
     void desktopRemoved(KWin::win::virtual_desktop* desktop);
 
-    /**
-     * Signal emitted whenever the current desktop changes.
-     * @param previousDesktop The virtual desktop changed from
-     * @param newDesktop The virtual desktop changed to
-     */
     void currentChanged(uint previousDesktop, uint newDesktop);
 
     /**
-     * Signal emmitted for realtime desktop switching animations.
-     * @param currentDesktop The current virtual desktop
-     * @param offset The current total change in desktop coordinate
-     * Offset x and y are negative if switching Left and Down.
-     * Example: x = 0.6 means 60% of the way to the desktop to the right.
+     * For realtime desktop switching animations. Offset is current total change in desktop
+     * coordinate. x and y are negative if switching left/down. Example: x = 0.6 means 60% of the
+     * way to the desktop to the right.
      */
     void currentChanging(uint currentDesktop, QPointF offset);
     void currentChangingCancelled();
 
-    /**
-     * Signal emitted whenever the desktop layout changes.
-     * @param columns The new number of columns in the layout
-     * @param rows The new number of rows in the layout
-     */
     void layoutChanged(int columns, int rows);
-    /**
-     * Signal emitted whenever the navigationWrappingAround property changes.
-     */
     void navigationWrappingAroundChanged();
 };
 
-/**
- * @brief Manages the number of available virtual desktops, the layout of those and which virtual
- * desktop is the current one.
- *
- * This manager is responsible for Virtual Desktop handling inside KWin. It has a property for the
- * count of available virtual desktops and a property for the currently active virtual desktop. All
- * changes to the number of virtual desktops and the current virtual desktop need to go through this
- * manager.
- *
- * On all changes a signal is emitted and interested parties should connect to the signal. The
- * manager itself does not interact with other parts of the system. E.g. it does not hide/show
- * windows of desktop changes. This is outside the scope of this manager.
- *
- * Internally the manager organizes the virtual desktops in a grid allowing to navigate over the
- * virtual desktops. For this a set of convenient methods are available which allow to get the id
- * of an adjacent desktop or to switch to an adjacent desktop. Interested parties should make use of
- * these methods and not replicate the logic to switch to the next desktop.
- */
 class KWIN_EXPORT virtual_desktop_manager
 {
 public:
     virtual_desktop_manager();
     ~virtual_desktop_manager();
 
-    /**
-     * @internal, for X11 case
-     */
     void setRootInfo(x11::net::root_info* info);
-    /**
-     * @internal
-     */
     void setConfig(KSharedConfig::Ptr config);
-    /**
-     * @returns Total number of desktops currently in existence.
-     * @see setCount
-     * @see countChanged
-     */
+
     uint count() const;
-    /**
-     * @returns the number of rows the layout has.
-     * @see setRows
-     * @see rowsChanged
-     */
     uint rows() const;
-    /**
-     * @returns The ID of the current desktop.
-     * @see setCurrent
-     * @see currentChanged
-     */
+
     uint current() const;
-    /**
-     * @returns The current desktop
-     * @see setCurrent
-     * @see currentChanged
-     */
     virtual_desktop* currentDesktop() const;
 
-    /**
-     * @returns The name of the @p desktop
-     */
     QString name(uint desktop) const;
-
-    /**
-     * @returns @c true if navigation at borders of layout wraps around, @c false otherwise
-     * @see setNavigationWrappingAround
-     * @see navigationWrappingAroundChanged
-     */
     bool isNavigationWrappingAround() const;
-
-    /**
-     * @returns The layout aware virtual desktop grid used by this manager.
-     */
     const virtual_desktop_grid& grid() const;
 
-    /**
-     * @returns The ID of the desktop above desktop @a id. Wraps around to the bottom of
-     * the layout if @a wrap is set. If @a id is not set use the current one.
-     */
     uint above(uint id = 0, bool wrap = true) const;
-    /**
-     * @returns The desktop above desktop @a desktop. Wraps around to the bottom of
-     * the layout if @a wrap is set. If @a desktop is @c null use the current one.
-     */
     virtual_desktop* above(virtual_desktop* desktop, bool wrap = true) const;
-    /**
-     * @returns The ID of the desktop to the right of desktop @a id. Wraps around to the
-     * left of the layout if @a wrap is set. If @a id is not set use the current one.
-     */
+
     uint toRight(uint id = 0, bool wrap = true) const;
-    /**
-     * @returns The desktop to the right of desktop @a desktop. Wraps around to the
-     * left of the layout if @a wrap is set. If @a desktop is @c null use the current one.
-     */
     virtual_desktop* toRight(virtual_desktop* desktop, bool wrap = true) const;
-    /**
-     * @returns The ID of the desktop below desktop @a id. Wraps around to the top of the
-     * layout if @a wrap is set. If @a id is not set use the current one.
-     */
+
     uint below(uint id = 0, bool wrap = true) const;
-    /**
-     * @returns The desktop below desktop @a desktop. Wraps around to the top of the
-     * layout if @a wrap is set. If @a desktop is @c null use the current one.
-     */
     virtual_desktop* below(virtual_desktop* desktop, bool wrap = true) const;
-    /**
-     * @returns The ID of the desktop to the left of desktop @a id. Wraps around to the
-     * right of the layout if @a wrap is set. If @a id is not set use the current one.
-     */
+
     uint toLeft(uint id = 0, bool wrap = true) const;
-    /**
-     * @returns The desktop to the left of desktop @a desktop. Wraps around to the
-     * right of the layout if @a wrap is set. If @a desktop is @c null use the current one.
-     */
     virtual_desktop* toLeft(virtual_desktop* desktop, bool wrap = true) const;
-    /**
-     * @returns The desktop after the desktop @a desktop. Wraps around to the first
-     * desktop if @a wrap is set. If @a desktop is @c null use the current desktop.
-     */
+
     virtual_desktop* next(virtual_desktop* desktop = nullptr, bool wrap = true) const;
     uint next(uint id = 0, bool wrap = true) const;
-    /**
-     * @returns The desktop in front of the desktop @a desktop. Wraps around to the
-     * last desktop if @a wrap is set. If @a desktop is @c null use the current desktop.
-     */
+
     virtual_desktop* previous(virtual_desktop* desktop = nullptr, bool wrap = true) const;
     uint previous(uint id = 0, bool wrap = true) const;
 
-    /**
-     * @returns all currently managed virtual_desktops
-     */
     QVector<virtual_desktop*> desktops() const
     {
         return m_desktops;
     }
 
-    /**
-     * @returns The virtual_desktop for the x11 @p id, if no such virtual_desktop @c null is
-     * returned
-     */
     virtual_desktop* desktopForX11Id(uint id) const;
-
-    /**
-     * @returns The virtual_desktop for the internal desktop string @p id, if no such
-     * virtual_desktop
-     * @c null is returned
-     */
     virtual_desktop* desktopForId(QString const& id) const;
 
     /**
-     * Create a new virtual desktop at the requested position.
-     * The difference with setCount is that setCount always adds new desktops at the end of the
-     * chain. The Id is automatically generated.
-     * @param position The position of the desktop. It should be in range [0, count].
-     * @param name The name for the new desktop, if empty the default name will be used.
-     * @returns the new virtual_desktop, nullptr if we reached the maximum number of desktops
+     * Create a new virtual desktop at the requested position. The difference with setCount is that
+     * setCount always adds new desktops at the end of the chain. The Id is automatically generated.
+     * @returns the new virtual_desktop, nullptr if we reached the maximum number of desktops.
      */
     virtual_desktop* createVirtualDesktop(uint position, QString const& name = QString());
 
-    /**
-     * Remove the virtual desktop identified by id, if it exists
-     * difference with setCount is that is possible to remove an arbitrary desktop,
-     * not only the last one.
-     * @param id the string id of the desktop to remove
-     */
     void removeVirtualDesktop(QString const& id);
     void removeVirtualDesktop(virtual_desktop* desktop);
 
-    /**
-     * Updates the net root info for new number of desktops
-     */
     void updateRootInfo();
-
-    /**
-     * @returns The maximum number of desktops that KWin supports.
-     */
     static uint maximum();
 
-    /**
-     * Set the number of available desktops to @a count. This function overrides any previous
-     * grid layout.
-     * There needs to be at least one virtual desktop and the new value is capped at the maximum
-     * number of desktops. A caller of this function cannot expect that the change has been applied.
-     * It is the callers responsibility to either check the numberOfDesktops or connect to the
-     * countChanged signal.
-     *
-     * In case the @ref current desktop is on a desktop higher than the new count, the current
-     * desktop is changed to be the new desktop with highest id. In that situation the signal
-     * desktopRemoved is emitted.
-     * @param count The new number of desktops to use
-     * @see count
-     * @see maximum
-     * @see countChanged
-     * @see desktopCreated
-     * @see desktopRemoved
-     */
     void setCount(uint count);
-    /**
-     * Set the current desktop to @a current.
-     * @returns True on success, false otherwise.
-     * @see current
-     * @see currentChanged
-     * @see moveTo
-     */
     bool setCurrent(uint current);
-    /**
-     * Set the current desktop to @a current.
-     * @returns True on success, false otherwise.
-     * @see current
-     * @see currentChanged
-     * @see moveTo
-     */
     bool setCurrent(virtual_desktop* current);
-    /**
-     * Updates the layout to a new number of rows. The number of columns will be calculated
-     * accordingly
-     */
+
     void setRows(uint rows);
-    /**
-     * Called from within setCount() to ensure the desktop layout is still valid.
-     */
     void updateLayout();
-    /**
-     * @param enabled wrapping around borders for navigation in desktop layout
-     * @see isNavigationWrappingAround
-     * @see navigationWrappingAroundChanged
-     */
     void setNavigationWrappingAround(bool enabled);
-    /**
-     * Loads number of desktops and names from configuration file
-     */
+
     void load();
-    /**
-     * Saves number of desktops and names to configuration file
-     */
     void save();
 
     std::unique_ptr<virtual_desktop_manager_qobject> qobject;
 
-    /**
-     * Common slot for all "Switch to Desktop n" shortcuts.
-     * This method uses the sender() method to access some data.
-     * DO NOT CALL DIRECTLY! ONLY TO BE USED FROM AN ACTION!
-     */
     void slotSwitchTo(QAction& action);
-    /**
-     * Slot for switch to next desktop action.
-     */
     void slotNext();
-    /**
-     * Slot for switch to previous desktop action.
-     */
     void slotPrevious();
-    /**
-     * Slot for switch to right desktop action.
-     */
     void slotRight();
-    /**
-     * Slot for switch to left desktop action.
-     */
     void slotLeft();
-    /**
-     * Slot for switch to desktop above action.
-     */
     void slotUp();
-    /**
-     * Slot for switch to desktop below action.
-     */
     void slotDown();
 
-    /* For gestured desktopSwitching
-     * Called when gesture ended, the thing that actually switches the desktop.
-     */
+    /// Called when gesture ended, the thing that actually switches the desktop.
     QAction* swipe_gesture_released_y() const
     {
         return m_swipeGestureReleasedY.get();
@@ -481,16 +235,12 @@ public:
     Wrapland::Server::PlasmaVirtualDesktopManager* m_virtualDesktopManagement{nullptr};
 
 private:
-    /// Returns new desktops.
     QList<virtual_desktop*> update_count(uint count);
-    /**
-     * Generate a desktop layout from EWMH _NET_DESKTOP_LAYOUT property parameters.
-     */
+
+    /// Generate a desktop layout from EWMH _NET_DESKTOP_LAYOUT property parameters.
     void
     setNETDesktopLayout(Qt::Orientation orientation, uint width, uint height, int startingCorner);
-    /**
-     * @returns A default name for the given @p desktop
-     */
+
     QString defaultName(int desktop) const;
 
     QVector<virtual_desktop*> m_desktops;
