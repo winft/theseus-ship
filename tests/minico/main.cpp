83f679d8dc2b898b41663f05a9a5c7bcb0705e0b
Roman Gilg <subdiff@gmail.com>
2023-12-03 17:47:19 +0100

    test: simplify Minico main function
    We can assume that signals can be ignored in this minimal compositor. Setting
    the translation domain might be needed for some effects, but for now let's
    remove this call too.

--- tests/minico/main.cpp
+++ tests/minico/main.cpp
@@ -5,13 +5,8 @@ SPDX-License-Identifier: GPL-2.0-or-later
 */
 #include <base/wayland/app_singleton.h>
 #include <base/wayland/platform.h>
-#include <desktop/platform.h>
 #include <input/wayland/platform.h>
 #include <script/platform.h>
-
-#include <KSignalHandler>
-#include <QApplication>
-#include <iostream>
 
 Q_IMPORT_PLUGIN(KWinIntegrationPlugin)
 Q_IMPORT_PLUGIN(KWindowSystemKWinPlugin)
@@ -21,31 +16,12 @@ int main(int argc, char* argv[])
 {
     using namespace KWin;
 
-    KLocalizedString::setApplicationDomain("kwin");
-
-    signal(SIGPIPE, SIG_IGN);
-
-    // ensure that no thread takes SIGUSR
-    sigset_t userSignals;
-    sigemptyset(&userSignals);
-    sigaddset(&userSignals, SIGUSR1);
-    sigaddset(&userSignals, SIGUSR2);
-    pthread_sigmask(SIG_BLOCK, &userSignals, nullptr);
-
-    KWin::base::wayland::app_singleton app(argc, argv);
-
-    KSignalHandler::self()->watchSignal(SIGTERM);
-    KSignalHandler::self()->watchSignal(SIGINT);
-    KSignalHandler::self()->watchSignal(SIGHUP);
-    QObject::connect(KSignalHandler::self(),
-                     &KSignalHandler::signalReceived,
-                     app.qapp.get(),
-                     &QCoreApplication::exit);
+    base::wayland::app_singleton app(argc, argv);
 
     using base_t = base::wayland::platform<>;
     base_t base({.config = base::config(KConfig::OpenFlag::FullConfig, "kwinft-minimalrc")});
-    base.options = base::create_options(base::operation_mode::wayland, base.config.main);
 
+    base.options = base::create_options(base::operation_mode::wayland, base.config.main);
     base.mod.render = std::make_unique<base_t::render_t>(base);
     base.mod.input = std::make_unique<input::wayland::platform<base_t>>(
         base, input::config(KConfig::NoGlobals));
