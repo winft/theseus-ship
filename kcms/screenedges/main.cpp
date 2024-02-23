434a04192eaa2b002f9dd45c8ce8cb3941eeee81
Roman Gilg <subdiff@gmail.com>
2024-02-23 15:01:19 +0100

    refactor: avoid QtDBus module include
    Reduces the compile time.

--- kcms/screenedges/main.cpp
+++ kcms/screenedges/main.cpp
@@ -17,7 +17,6 @@ SPDX-License-Identifier: GPL-2.0-or-later
 #include <KPackage/PackageLoader>
 #include <KPluginFactory>
 #include <QVBoxLayout>
-#include <QtDBus>
 
 #include "kwinscreenedgeconfigform.h"
 #include "kwinscreenedgedata.h"
